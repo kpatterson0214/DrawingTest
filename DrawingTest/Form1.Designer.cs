dd9c2f6cd000a430f40302806e2da0d25078e23c
Keith Patterson <kpatterson@kmpdesigns.com>
2016-12-11 06:04:47 -0500

    Add project files.

---
+++ DrawingTest/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace DrawingTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdDraw = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmdDraw
+            // 
+            this.cmdDraw.Location = new System.Drawing.Point(76, 484);
+            this.cmdDraw.Name = "cmdDraw";
+            this.cmdDraw.Size = new System.Drawing.Size(188, 48);
+            this.cmdDraw.TabIndex = 1;
+            this.cmdDraw.Text = "Draw..";
+            this.cmdDraw.UseVisualStyleBackColor = true;
+            this.cmdDraw.Click += new System.EventHandler(this.cmdDraw_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(793, 573);
+            this.Controls.Add(this.cmdDraw);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button cmdDraw;
+    }
+}
+
---
+++ DrawingTest/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawingTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void DrawEllipse()
+        {
+            System.Drawing.Pen myPen;
+            myPen = new System.Drawing.Pen(System.Drawing.Color.Black);
+            System.Drawing.Graphics formGraphics = this.CreateGraphics();
+            formGraphics.DrawEllipse(myPen, new Rectangle(0, 0, 200, 200));
+            myPen.Dispose();
+            formGraphics.Dispose();
+        }
+        private void DrawRectangle()
+        {
+            System.Drawing.Pen myPen;
+            myPen = new System.Drawing.Pen(System.Drawing.Color.Red);
+            System.Drawing.Graphics formGraphics = this.CreateGraphics();
+            formGraphics.DrawRectangle(myPen, new Rectangle(0, 0, 200, 200));
+            myPen.Dispose();
+            formGraphics.Dispose();
+        }
+        private void DrawLine()
+        {
+            System.Drawing.Pen myPen;
+            myPen = new System.Drawing.Pen(System.Drawing.Color.Blue);
+            System.Drawing.Graphics formGraphics = this.CreateGraphics();
+            formGraphics.DrawLine(myPen, 0, 0, 200, 200);
+            myPen.Dispose();
+            formGraphics.Dispose();
+        }
+        private void cmdDraw_Click(object sender, EventArgs e)
+        {
+            DrawEllipse();
+            DrawRectangle();
+            DrawLine();
+        }
+
+    }
+}
